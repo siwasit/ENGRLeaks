1133e5b1f595216f6a1425465292e3a2e0328550
Worayot-MAC <worayot@WorayotMAC.local>
2025-05-11 19:31:16 +0700

    Added csrf token value to getcsrftokenfromcookies method

--- frontend/src/utils/getCsrfToken.tsx
+++ frontend/src/utils/getCsrfToken.tsx
@@ -12,7 +12,7 @@ export const getCsrfTokenFromCookies = () => {
     for (const cookie of cookies) {
         const [key, value] = cookie.trim().split('=');
         if (key === cookieName) {
-            console.log(`CSRF token found`);
+            console.log(`CSRF token found: ${value}`);
             return value;
         }
     }
