5a0312b0505ba507f48d30b86fd121cec3954ee7
Worayot-MAC <worayot@WorayotMAC.local>
2025-05-11 22:35:33 +0700

    Commented out .onrender.com

--- backend/backend_app/backend_app/settings.py
+++ backend/backend_app/backend_app/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -30,7 +31,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
@@ -172,8 +172,8 @@ CORS_ALLOW_CREDENTIALS = True
 
 #* Production settings
 DEBUG = False 
-CSRF_COOKIE_DOMAIN = ".onrender.com" 
-SESSION_COOKIE_DOMAIN = ".onrender.com"
+# CSRF_COOKIE_DOMAIN = ".onrender.com" 
+# SESSION_COOKIE_DOMAIN = ".onrender.com"
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'    
